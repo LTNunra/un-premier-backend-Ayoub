173f3f20fe3cd5737824702e22b08244b067eaf5
yo <ledicodeyo@gmail.com>
2025-09-12 17:09:53 -0400

    Serveur et controleurs

---
+++ src/main/java/cal/info/ControleurHackathon.java
@@ -0,0 +1,13 @@
+package cal.info;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class ControleurHackathon implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        System.out.println("Allo Valentin");
+    }
+}
