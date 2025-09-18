383e5030f331100a2920adf7cae108ef6b894c96
yo <ledicodeyo@gmail.com>
2025-09-18 13:23:17 -0400

    instancie la liste et retrouve donnees

--- src/main/java/cal/info/GestionHackathon.java
+++ src/main/java/cal/info/GestionHackathon.java
@@ -1,12 +1,17 @@
 package cal.info;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GestionHackathon {
-    public List<Hackathon> hackathons;
+    public List<Hackathon> hackathons = new ArrayList();
 
     public void ajouterHackathon(Hackathon hackathon) {
         hackathons.add(hackathon);
+    }
+
+    public List<Hackathon> retrouverHackathons() {
+        return hackathons;
     }
 
     public void modifierHackathon(Hackathon hackathon) {
